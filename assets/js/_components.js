0b94240d12567d35516186e26817d2ece7a08b58
unknown <iamgodsgiftmatthew@gmail.com>
2021-10-19 12:33:35 -0700

    feat & new: updated nav style, created services pg, trades pg, help pg, etc

--- assets/js/_components.js
+++ assets/js/_components.js
@@ -1,67 +1,35 @@
-"use strict";
+'use strict'
 
-$(".switch-field-label").click(function() {
-    const label = $(this);
-    const viewPort = window.innerWidth;
-    const cardList = $('.cardlist-container').find('.card');
-    const isGalleryView = label.attr('card-view') == 'gallery' ? true : false;
-    const isMediumUp = viewPort >= 768 ? true : false;
-    let viewMode = 'card-list-view';
-    const isMediumScreenUp = viewPort >= 768 ? true : false;
-    
-    let rmCol = 'col-md-12 col-lg-8';
-    let adCol = 'col-md-6 col-lg-4';
+$('.switch-field-label').click(function () {
+    const label = $(this)
+    const main = label.parents('main')
+    const cardContainer = $('.cardlist-container')
+    const cardList = cardContainer.find('.card')
+    const isGalleryView = label.attr('card-view') == 'gallery' ? true : false
 
-    if (isGalleryView) {
-        if (isMediumUp) {
-            rmCol = 'col-md-12 col-lg-8';
-            adCol = 'col-md-6 col-lg-4';
-            viewMode = '';
-        }
+    let gridTemp = cardContainer.attr('grid-template')
+    let listTemp = cardContainer.attr('list-template')
+    let addCol = gridTemp
+    let rmCol = listTemp
+
+    if (!isGalleryView) {
+        addCol = listTemp
+        rmCol = gridTemp
     }
 
     for (const card of cardList) {
-        const column = $(card).parent();
-        column.removeClass(rmCol).addClass(adCol);
-        $(card)[isGalleryView & isMediumUp  ? 'removeClass' : 'addClass'](viewMode);
+        const column = $(card).parent()
+        column.removeClass(rmCol).addClass(addCol)
+        $(card)[isGalleryView ? 'removeClass' : 'addClass']('card-list-view')
     }
-    
-    
-    
-    // if (!isGalleryView) {
-    //     for (const card of cardList) {
-    //         const column = $(card).parent();
-    //         column.removeClass(`col-lg-4 ${isMediumScreenUp ?? 'col-md-6'}`).addClass(`col-lg-8 ${isMediumScreenUp ?? 'col-md-8'}`);
-    //         $(card).addClass('card-list-view');
-    //     }
-    //     return;  
-    // }
-    
 })
-// $(".switch-field-label").click(function() {
-//     const label = $(this);
-//     const cardList = $('.cardlist-container').find('.card');
-//     const isGalleryView = label.attr('card-view') == 'gallery' ? true : false;
-//     const viewPort = window.innerWidth;
-//     const isMediumScreenUp = viewPort >= 768 ? true : false;
-//     // const isMediumScreenDown = viewPort >= 768 ? true : false;
-//     const columnList = [4,6,8];
-//     // let columnCode = isGalleryView ? 4 : 8;
-    
-    
-    
-    
-//     if (!isGalleryView) {
-//         for (const card of cardList) {
-//             const column = $(card).parent();
-//             column.removeClass(`col-lg-4 ${isMediumScreenUp ?? 'col-md-6'}`).addClass(`col-lg-8 ${isMediumScreenUp ?? 'col-md-8'}`);
-//             $(card).addClass('card-list-view');
-//         }
-//         return;  
-//     }
-//     for (const card of cardList) {
-//         const column = $(card).parent();
-//         column.removeClass('col-lg-8').addClass('col-lg-4');
-//         $(card).removeClass('card-list-view');
-//     }
-// })
+
+$('#back_to_top-btn').click(function () {
+    $('body,html').animate(
+        {
+            scrollTop: 0,
+        },
+        400
+    )
+    return false
+})
