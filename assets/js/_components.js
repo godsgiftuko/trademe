ceb4ae3826c4471fcea06badc479bd2df6749a4b
thegodsgift <iamgodsgiftmatthew@gmail.com>
2021-11-15 08:43:31 +0100

    chores: motor pg 70%

--- assets/js/_components.js
+++ assets/js/_components.js
@@ -1,21 +1,15 @@
 'use strict'
 
 $('.switch-field-label').click(function () {
-    const label = $(this)
-    const main = label.parents('main')
-    const cardContainer = $('.cardlist-container')
-    const cardList = cardContainer.find('.card')
-    const isGalleryView = label.attr('card-view') == 'gallery' ? true : false
+    const label = $(this);
+    const cardContainer = $('.cardlist-container');
+    const cardList = cardContainer.find('.card');
+    const isGalleryView = label.attr('card-view') == 'gallery' ? true : false;
 
-    let gridTemp = cardContainer.attr('grid-template')
-    let listTemp = cardContainer.attr('list-template')
-    let addCol = gridTemp
-    let rmCol = listTemp
-
-    if (!isGalleryView) {
-        addCol = listTemp
-        rmCol = gridTemp
-    }
+    let gridTemp = cardContainer.attr('grid-template');
+    let listTemp = cardContainer.attr('list-template');
+    let addCol = !isGalleryView ? listTemp : gridTemp;
+    let rmCol = !isGalleryView ? gridTemp : listTemp;
 
     for (const card of cardList) {
         const column = $(card).parent()
@@ -23,6 +17,7 @@ $('.switch-field-label').click(function () {
         $(card)[isGalleryView ? 'removeClass' : 'addClass']('card-list-view')
     }
 })
+
 
 $('#back_to_top-btn').click(function () {
     $('body,html').animate(
---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$('#moreOpt').click(function(e) {
+    const optContent = $('.moreOpt-content');
+    $(this).find('span').text(!optContent.hasClass('d-none') ? 'More options' : 'Fewer options');
+    $(this).find('i').toggleClass('rotate-180');
+    optContent.toggleClass('d-none');
+})
+
+$('#my_bottom_tab li').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+    const _parents = $(this).parents('.hero_bottom_nav');
+    const actBtn = _parents.find('button');
+    const selects = _parents.find('.search_box_form select');
+    
+    // actBtn.text( ? actBtn.text())
+    // data-id="odometer"
+
+    // for (const select of selects) {
+    //     console.log(selects)
+    // }
+    // console.log(odometer);
+})
